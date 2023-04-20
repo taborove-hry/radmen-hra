83bf33d46d0a1ad03ea7e5ba31013e513a880661
Vojtěch Tranta <vojta.tranta@gmail.com>
2023-04-20 21:16:34 +0200

    fasten background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen min-w-screen flex-1 flex-col items-center justify-center p-10" style={{background: 'black', backgroundImage: `url(https://drive.google.com/uc?id=1QgdBViD-FWMY3DtYeW5teevt1nhSsPKX)`, backgroundPosition: 'top center', backgroundSize: 'cover' }}>
+    <main className="flex min-h-screen min-w-screen flex-1 flex-col items-center justify-center p-10" style={{background: 'black', backgroundImage: `url(https://djaccw3ms0b81.cloudfront.net/zbytecnosti/bckg.jpg)`, backgroundPosition: 'top center', backgroundSize: 'cover' }}>
       <div className="z-10 w-full justify-self-end justify-center font-mono lg:flex" >
 
        <div className='justify-self-end flex-1' style={{maxWidth: '640px'}}><Form /></div>
