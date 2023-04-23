adfc15522c2b066076b6b9ffbfa142f297e72fd1
Vojtěch Tranta <vojta.tranta@gmail.com>
2023-04-23 17:11:30 +0200

    add country flags and texts

--- src/utils/validate-password.ts
+++ src/utils/validate-password.ts
@@ -3,7 +3,7 @@ export type Country = { password: string, flag?:string; slug: string, country: s
 export const Britain: Country = {
   password: 'God save the king',
   slug: 'united-kingdom-of-great-britain-and-northern-ireland',
-  country: 'United Kingdom of Great Britain and Northern Ireland',
+  country: 'United Kingdom of Great Britain and Ireland',
   flag: 'https://djaccw3ms0b81.cloudfront.net/zbytecnosti/britainflag.png',
   images: ['https://djaccw3ms0b81.cloudfront.net/zbytecnosti/britan.jpg'],
   html: `
@@ -59,37 +59,302 @@ export const Britain: Country = {
 export const Germany: Country = {
   password: 'Es lebe der Kaiser',
   slug: 'deutsches-kaiserreich',
-  country: 'Germany',
+  country: 'Deutsches Kaiserreich',
+  flag: 'https://djaccw3ms0b81.cloudfront.net/zbytecnosti/germanyflag.png',
+  html: `<p style="margin: 1em 0">
+  Vážený diplomate,
+</p>
+<p style="margin: 1em 0">
+  vaše účast na této konferenci bude velmi důležitá, neboť Německo má v
+  současné době klíčovou roli v evropské politice a v budoucím uspořádání
+  světa.
+</p>
+<p style="margin: 1em 0">
+  Německo se v průběhu posledních let vyvinulo do jedné z nejvýznamnějších
+  evropských mocností a získalo silné postavení jak v Evropě, tak v
+  celosvětovém měřítku. Naše země je klíčovým hráčem v evropské politice a má
+  zásadní vliv na celkové směřování kontinentu.
+</p>
+<p style="margin: 1em 0">
+  Vzhledem k tomu, že nové uspořádání světa je v současné době klíčovou
+  otázkou pro celou Evropu a svět, je velmi důležité, aby Německo bylo
+  aktivně zapojeno do diskuzí a rozhodování.
+</p>
+<p style="margin: 1em 0">
+  Naše země může hrát klíčovou roli v budoucím uspořádání Evropy a světa a
+  Vaše účast na této konferenci bude důležitá pro to, aby byl Německu
+  poskytnut hlas v těchto klíčových otázkách.
+</p>
+<p style="margin: 1em 0">
+  Německo je země, která má silnou historii a kulturní odkaz. Naše země je
+  synonymem pro inovaci, průmyslový vývoj a kulturní obohacení.
+</p>
+<p style="margin: 1em 0">
+  Jak se však naše pozice může zdát výhodná pro možnou expanzi všemi směry,
+  musíme se míti i na pozoru. Rakousko-Uhersko s námi může na první pohled
+  sympatizovat, nicméně musíme v Evropě najít dalšího silného spojence v
+  případě mezinárodního konfliktu.
+</p>
+<p style="margin: 1em 0">
+  Věřím, že u našich dalších sousedů jsou možné příležitosti k tomu, aby se
+  misky vah naklonili na naši stranu. To ale přenechám vašemu diplomatickému
+  umu.
+</p>
+<p style="margin: 1em 0">
+  Německo je připraveno, dá se to samé říci i o vás?
+</p>
+<p style="margin: 1em 0">
+  Přesvědčte mě
+</p>
+<p style="margin: 1em 0">
+  S úctou
+</p>
+<p style="margin: 1em 0">
+  Vilém II. Pruský
+</p>`,
 }
 
 export const Austria: Country = {
   password: 'Es lebe der Konig und Kaiser',
   slug: 'osterreich-ungarn',
-  country: 'Austria-Hungary',
+  country: 'Österreichisch-Ungarische Monarchie',
+  flag: 'https://djaccw3ms0b81.cloudfront.net/zbytecnosti/austriahungaryflag.png',
+  html: `<p style="margin: 1em 0">
+  Vážený diplomate,
+</p>
+<p style="margin: 1em 0">
+  s velkou nadějí Vám píši s vědomím historického momentu, ve kterém se naše
+  země ocitla. Rád bych Vám předal informace o důvodech, proč by měla
+  Rakousko-Uherská země vstoupit nadcházejícího konfliktu.
+</p>
+<p style="margin: 1em 0">
+  Náš národ se nachází na křižovatce nelehkých rozhodnutí, ale jedno je
+  jisté: musíme hájit naše zájmy a zachovat naši suverenitu. Rakousko-Uhersko
+  čelí mnoha hrozbám a výzvám, a právě v této chvíli se musíme postavit proti
+  našim nepřátelům.
+</p>
+<p style="margin: 1em 0">
+  Jedním z nejdůležitějších důvodů, proč musíme jednat je ochrana našich
+  hranic a národní bezpečnosti. Vstupem do stavu válečného bychom ukázali
+  naši sílu a odhodlání bránit naše území a naši národní identitu.
+</p>
+<p style="margin: 1em 0">
+  V případě, že bychom tak neučinili, naše země by se mohla dostat do izolace
+  a stali bychom se snadným terčem našich nepřátel.
+</p>
+<p style="margin: 1em 0">
+  Nemůžeme také zapomenout na naše ekonomické zájmy a potřeby. Vstupem do
+  stavu válečného bychom si zajistili kontrolu nad klíčovými obchodními
+  cestami a zdroji surovin, což by mělo pozitivní vliv na naši ekonomiku.
+</p>
+<p style="margin: 1em 0">
+  Německo sice může posloužit se svou ekonomikou jako opěrný bod, ale pokud
+  nad námi získá velkou převahu, může to být nakonec náš potencionální
+  spojenec, který nás bude ohrožovat. Ovšem ani Ruský Car nebo Turecký Sultán
+  nesmí být brán na lehkou váhu. O Francii a Anglii ani nemluvě. Nemáme
+  jednoduché postavení a budete muset vynaložit veškeré své diplomatické
+  dovednosti, abyste Rakousko-Uhersko dosadil na výsluní Evropy, kam patří.
+</p>
+<p style="margin: 1em 0">
+  Musíme ukázali světu, že jsme silný a nezlomný národ, který neváhá hájit
+  své zájmy a bránit svou suverenitu.
+</p>
+<p style="margin: 1em 0">
+  S úctou,
+</p>
+<p style="margin: 1em 0">
+  Císař František Josef I.
+</p>`
 }
 
 export const Russia: Country = {
   password: 'Da zdravstvuyet tsar',
   slug: 'rossiyskaya-imperiya',
-  country: 'Russia',
+  flag: 'https://djaccw3ms0b81.cloudfront.net/zbytecnosti/russianflag.png',
+  country: 'Rossiyskaya Imperiya',
+  html: `<p style="margin: 1em 0">
+  Setkání na které se dostavíte bude mít velký význam pro osud naší země, ale
+  i celého světa. Jako zástupce velkého Ruska, byste neměl tuto příležitost
+  opomenout.
+</p>
+<p style="margin: 1em 0">
+  Jak jistě víte, situace v Rusku je nyní velmi nestabilní a mnoho lidí se
+  obává o budoucnost naší země. Víme, že mnoho Rusů má pocit, že naše země
+  stojí na pokraji zkázy a potřebujeme jednotu a silné vedení, abychom
+  překonali tyto těžké časy. Je proto potřeba najít cestu k lepší
+  budoucnosti.
+</p>
+<p style="margin: 1em 0">
+  V nadcházejícím konfliktu by mělo Rusko zaujmout důležité místo.
+  Koneckonců, jsme jednou z největších zemí na světě a vaše přítomnost v
+  jednání může znamenat rozdíl mezi vítězstvím a porážkou.
+</p>
+<p style="margin: 1em 0">
+  Víme, že v Rusku existuje velké množství nadšených a oddaných lidí, kteří
+  by byli rádi, kdyby mohli sloužit své vlasti. A toho je potřeba využít.
+</p>
+<p style="margin: 1em 0">
+  Ve chvíli, kdy bude v Evropě zažehnut konflikt, je nutné, abychom byli
+  připraveni. Mnoho našich sousedů by rádo expandovalo a naše velká hranice
+  jim v tomto případě může poskytnout zajímavou příležitost ke kořisti. V
+  Evropě kromě Francouzů nemáme zastání, nehledě na to, že spoléhat se na
+  Francouze by jeden mohl považovat za čirou bláznivost spjatou se
+  zoufalstvím.
+</p>
+<p style="margin: 1em 0">
+  Než se konflikt rozhoří, je zapotřebí setkat se s dalšími lidmi, kteří
+  budou mít podobné smýšlení a vytvoří s námi silné spojenectví pro společné
+  cíle.
+</p>
+<p style="margin: 1em 0">
+  Věříme, že se naše země a náš lid může vypořádat s výzvami, které nám
+  nadcházející konflikt přinese.
+</p>
+<p style="margin: 1em 0">
+  Bojujme společně, jako jedna velká rodina.
+</p>
+<p style="margin: 1em 0">
+  S úctou,
+</p>
+<p style="margin: 1em 0">
+  Car Mikuláš II.
+</p>`
 }
 
 export const Italy: Country = {
   password: 'Viva il Re',
   slug: 'regno-ditalia',
-  country: 'Italy',
+  country: 'Regno d\'Italia',
+  flag: 'https://djaccw3ms0b81.cloudfront.net/zbytecnosti/italyflag.png',
+  html: `<p style="margin: 1em 0">
+  Vážený diplomate,
+</p>
+<p style="margin: 1em 0">
+  k nadcházejícímu konfliktu nás sice nezavazuje žádná z našich smluv, a tak
+  můžeme zůstat stranou jako pozorovatel. Avšak, bylo by obrovskou chybou
+  nevyužít případný chaos v Evropě pro expanzi naší země.
+</p>
+<p style="margin: 1em 0">
+  Itálie se musí zapojit do tohoto konfliktu. Naše země se již příliš dlouho
+  potácí v pozadí dějin a my musíme dokázat, že jsme rozhodnuti stát se
+  skutečnou velmocí. Je naší povinností sloužit naší vlasti a ukázat světu,
+  že Itálie má své místo mezi velkými mocnostmi.
+</p>
+<p style="margin: 1em 0">
+  Naši předkové bojovali tvrdě, aby nám zanechali zemi, na kterou jsme pyšní.
+  Je na nás, abychom ochránili jejich dědictví a ukázali, že jsme stejně
+  stateční a oddaní jako oni. Dejte ukázat světu, že Itálie je schopná stát
+  se nejen kulturní, ale i politickou a vojenskou silou.
+</p>
+<p style="margin: 1em 0">
+  Rakousko-Uhersko si možná může myslet, že naše pozornost pro případnou
+  expanzi se bude stáčet na Africké pobřeží, ale to může být jenom zlomek
+  našich velkých cílů. Neboť pomocí našeho silného námořnictva i Balkánské
+  země mohou být příhodno k rozšíření našeho impéria. Pokud Rakousko-Uhersko
+  a Francie promarní příležitost a nechá se vlákat do pasti, která bude mít
+  za důsledek jejich oslabení v regionech blízko našich hranic, i toho bychom
+  měli využít v náš prospěch.
+</p>
+<p style="margin: 1em 0">
+  Věříme, že se vám podaří dosáhnout úspěchu na bojišti a prokázat světu, že
+  jsme silní a odhodlaní.
+</p>
+<p style="margin: 1em 0">
+  Uděláme pro naši zemi a pro naše lidi všechno, abychom zajistili jejich
+  budoucnost a stabilitu, kterou si zaslouží.
+</p>
+<p style="margin: 1em 0">
+  S úctou,
+</p>
+<p style="margin: 1em 0">
+  Antonio Salandra
+</p>`
 }
 
 export const Turkey: Country = {
   password: 'Yasasin Sultanimiz',
   slug: 'osmanli-imparatorlugu',
-  country: 'Turkey',
+  country: 'Devlet-i ʿAlīye-i ʿOsmānīye',
+  flag: 'https://djaccw3ms0b81.cloudfront.net/zbytecnosti/turkeyflag.png',
+  html: `<p style="margin: 1em 0">
+  Vážený diplomate,
+</p>
+<p style="margin: 1em 0">
+  jistě jste si vědom situace, která nyní panuje v Evropě a celém světě. Je
+  to doba, kdy naše vlast potřebuje své nejlepší syny a dcery k tomu, aby
+  hájili naše svobody a zachovali naši kulturu před nebezpečím, které jí
+  hrozí.
+</p>
+<p style="margin: 1em 0">
+  Turecko má před sebou důležitou volbu a příležitost. Víme, že jste hrdý na
+  svou zemi a na svou historii a že chcete, aby Turecko bylo vnímáno jako
+  velký a respektovaný hráč na světové scéně. Vaše účast by tedy mohla být
+  jednou z nejlepších příležitostí, jak tohoto cíle dosáhnout.
+</p>
+<p style="margin: 1em 0">
+  Tak velký konflikt, který je již v Evropě na spadnutí, je důležitým
+  okamžikem v historii, který ovlivní nejen evropské země, ale celý svět. Je
+  to událost, která ukáže sílu národů, ale také to, jak rychle může být
+  zničen. Turecko by mohlo mít velký vliv na to, jak se tento konflikt vyvine
+  a jaké budou jeho výsledky.
+</p>
+<p style="margin: 1em 0">
+  Naše země má bohatou historii a kulturu, která by mohla přinést mnoho
+  prospěchu v této válce. Vaše odvaha, statečnost a schopnosti by mohly být
+  klíčovými faktory pro úspěch v nadcházejícím konfliktu.
+</p>
+<p style="margin: 1em 0">
+  Přeji mnoho zdaru
+</p>
+<p style="margin: 1em 0">
+  S pozdravem a nadějí na lepší budoucnost,
+</p>
+<p style="margin: 1em 0">
+  Sultan Mehmed V.
+</p>`
 }
 
 export const France: Country = {
   password: 'Vive la Republique',
   slug: 'troisieme-republique-francaise',
   country: 'France',
+  flag: 'https://djaccw3ms0b81.cloudfront.net/zbytecnosti/franceflag.png',
+  html: `<p style="margin: 1em 0;">
+  Vážený diplomate,
+</p>
+<p style="margin: 1em 0;">
+  Samotné důvody, proč by Francie měla vstoupit do nadcházejícího konfliktu,
+  jsou samozřejmě složité a mnohoznačné. Nicméně, následující body mohou
+  poskytnout určitý přehled o důvodech, které jsou pro nás důležité:
+</p>
+<p style="margin: 1em 0;">
+  Obrana Francouzské republiky: Francouzská republika je státem, který si
+  udržuje silnou národní identitu a občanskou vlasteneckou tradici. Případný
+  útok na Francii by byl vnímán jako ohrožení této republiky a jejího
+  systému. Francouzský lid se cítí povinný chránit svou vlast před případnou
+  eskalací ať přijde z jakékoliv strany našich hranic.
+</p>
+<p style="margin: 1em 0;">
+  Vztahy s Velkou Británií a Ruskem: Máme ve světě sice důležité spojenecké
+  vztahy s Velkou Británií a Ruskem, ale nikdy nevíte, na koho se můžeme
+  spolehnout. Německo a Rakousko-Uhersko pro nás sice momentálně představují
+  určitý druh hrozby, nicméně nesmíme zapomenout, že s námi Anglický soused
+  ne vždycky sympatizoval a Rusovi nemůžeme věřit ani když se stébla chytá.
+  Určitá vzdálenost je však pro nás výhodou.
+</p>
+<p style="margin: 1em 0;">
+  Revanchismus: Francouzská společnost je ovlivněna myšlenkou revanšismu,
+  tedy snahou o odvetu a obnovu francouzské moci po porážce v
+  Prusko-francouzské válce v roce 1870. Někteří Francouzi věří, že vstup do
+  nadcházejícího konfliktu by mohl být příležitostí k obnovení francouzského
+  vlivu a prestiže v Evropě.
+</p>
+<p style="margin: 1em 0;">
+  S pozdravem a nadějí na lepší budoucnost,
+</p>
+<p style="margin: 1em 0;">
+  Prezident Raymond Poincaré
+</p>`
 }
 
 
